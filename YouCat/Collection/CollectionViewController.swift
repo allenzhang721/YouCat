af019cdec09cfe50f6c627dec79f1594484c8bb8
Emiaostein <cszwdy@gmail.com>
2018-03-29 11:17:26 +0800

    1. Update cell style.

--- YouCat/Collection/CollectionViewController.swift
+++ YouCat/Collection/CollectionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CollectionViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,5 +34,39 @@ class CollectionViewController: UIViewController {
     */
 
 }
+
+extension CollectionViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        
+        
+        
+        let label = cell.viewWithTag(100) as! UILabel
+        let shadow = cell.viewWithTag(300)!
+        
+        
+        label.text = "Basic background Cosmetic surgery consists not just of enhancing someones beauty, but also to help those who have been badly damaged in an accident or who have physical birth defects. Many believe that women are the main gender that go for cosmetic surgery. "
+        
+        cell.layoutIfNeeded()
+        
+//        shadow.layer.shadowPath = UIBezierPath(roundedRect: shadow.bounds, cornerRadius: 8).cgPath
+        shadow.layer.shadowOffset = CGSize(width: 0, height: 2)
+        shadow.layer.shadowRadius = 10
+        shadow.layer.shadowColor = UIColor.black.cgColor
+        shadow.layer.shadowOpacity = 0.1
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+}
+
 
 
